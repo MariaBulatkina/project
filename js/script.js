5c8ff15fb1d781a924cf321970c20be181c2c4d5
Maria <marya.bulatkina@yandex.ru>
2022-02-06 09:49:35 +0300

    first massage

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?');
+answers[1] = prompt('Сколько вам лет?');
+answers[2] = prompt('Где вы живете');
+
+document.write(answers);
+
+//вся инфа от пользователя в виде строк
